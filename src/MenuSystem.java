9e701f3c3deb76b584cfee85573aa1b6801278d2
pcolasante <118872813+pcolasante@users.noreply.github.com>
2026-03-10 01:22:42 -0400

    new menu class, main was getting crowed

---
+++ src/MenuSystem.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+
+public class MenuSystem {
+
+    private Scanner sc;
+    public MenuSystem() {
+        sc = new Scanner(System.in);
+    }
+
+    public int menu() {
+
+    }
+
+}
