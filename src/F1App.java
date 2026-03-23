3ec8d43b5267a91f2596ebd678832bf27804e2b0
pcolasante <118872813+pcolasante@users.noreply.github.com>
2026-03-23 00:13:49 -0400

    Updated File Manager, Information Manager, Menu System and F1App main class. Now needs... error resolution, some refactoring too.

--- src/F1App.java
+++ src/F1App.java
@@ -11,137 +11,29 @@
  Class: F1App (Main class): Will launch the main console-based application.
 
  */
-
-import java.util.Scanner;
-
 public class F1App {
+
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        InformationManager manager = new InformationManager();
+        DataValidation validator = new DataValidation();
 
-        int selectChoice;
+        FileManager fileManager =
+                new FileManager();
 
-        do {
-            //Sets the main menu
-            System.out.println("\n--- FORMULA 1 - DATA MANAGEMENT SYSTEM ---\n");
-            System.out.println("1. Drivers");
-            System.out.println("2. Races");
-            System.out.println("3. Championship Standings");
-            System.out.println("4. Exit");
-            System.out.print("Select an option: ");
+        InformationManager manager =
+                fileManager.loadFromFile();
 
-            selectChoice = scanner.nextInt();
-            scanner.nextLine(); // clear buffer
-            switch (selectChoice) {
-                case 1:
-                    int choice1;
-                    do {
-                        System.out.println("\n--- Drivers ---\n");
-                        System.out.println("1. View Drivers");
-                        System.out.println("2. Add Driver");
-                        System.out.println("3. Update Driver");
-                        System.out.println("4. Delete Driver");
-                        System.out.println("5. Load Driver from file");
-                        System.out.println("6. Return to Main Menu");
-                        System.out.print("Select an option: ");
+        ChampionshipCalculator calculator =
+                new ChampionshipCalculator();
 
-                        choice1 = scanner.nextInt();
-                        scanner.nextLine();
-                        switch (choice1) {
-                            case 1:
-                                //Displays Drivers
-                                System.out.println("\n--- Drivers ---\n");
-                                manager.getAllDrivers();
-                                break;
-                            case 2:
-                                //Adds a Driver
-                                System.out.println("\n--- Add a Driver ---\n");
-                                manager.addDriver();
-                                break;
-                            case 3:
-                                //Update a Driver
-                                System.out.println("\n--- Update a Driver ---\n");
-                                manager.updateDriver();
-                                break;
-                            case 4:
-                                //Delete a Driver
-                                System.out.println("\n--- Delete a Driver ---\n");
-                                manager.removeDriver();
-                                break;
-                            case 5:
-                                System.out.println("\n--- Load Driver from file ---\n");
-                                manager.loadDriver();
-                                break;
-                            case 6:
-                                System.out.println("\n--- Return to Main Menu ---\n");
-                                break;
-                            default:
-                                System.out.println("\n--- Invalid option ---\n");
-                        }
-                    } while (choice1 != 6);
-                    break;
-//---------------------------------------------------------------------------------------------------//
-                case 2:
-                    int choice2;
-                    do {
-                        System.out.println("\n--- Races ---\n");
-                        System.out.println("1. View Races");
-                        System.out.println("2. Add Race");
-                        System.out.println("3. Update Race");
-                        System.out.println("4. Delete Race");
-                        System.out.println("5. Load Race from file");
-                        System.out.println("6. Return to Main Menu");
-                        System.out.print("Select an option: ");
+        MenuSystem menu =
+                new MenuSystem(manager, fileManager, validator, calculator);
 
-                        choice2 = scanner.nextInt();
-                        scanner.nextLine();
-                        switch (choice2) {
-                            case 1:
-                                System.out.println("\n--- Races ---\n");
-                                manager.getAllRaces();
-                                break;
-                            case 2:
-                                System.out.println("\n--- Add a Race ---\n");
-                                manager.addRace();
-                                break;
-                            case 3:
-                                System.out.println("\n--- Update a Race ---\n");
-                                manager.updateRace();
-                                break;
-                            case 4:
-                                System.out.println("\n--- Delete a Race ---\n");
-                                manager.deleteRace();
-                                break;
-                            case 5:
-                                System.out.println("\n--- Load Race from file ---\n");
-                                manager.loadRace();
-                                break;
-                            case 6:
-                                System.out.println("\n--- Return to Main Menu ---\n");
-                                break;
-                            default:
-                                System.out.println("\n--- Invalid option ---\n");
-                        }
-                    } while (choice2 != 6);
-                    break;
+        boolean saved = menu.start();
 
-//--------------------------------------------------------------------------------------------------//
-                case 3:
-                    System.out.println("\n--- Championship Standings ---\n");
-                    //what the fuck am i doing here
-                    break;
-                case 4:
-                    System.out.println("EXITING FORMULA 1 DATA MANAGEMENT SYSTEM");
-                    break;
-                default:
-                    System.out.println("INVALID SELECTION");
-            }
-
+        if (!saved) {
+            System.out.println("Error saving data.");
         }
-        while (selectChoice != 4);
-
-        scanner.close();
-
     }
+
 }
